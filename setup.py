cbcb98c5b48b7f5b090ea99eb6a11cf8beece313
leandrors <leandrors@ciandt.com>
2020-07-15 11:03:52 -0300

    Adding requirements to setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,15 @@ from crosslangt import VERSION
 setup(
     name="crosslangt",
     packages=find_packages(),
-    version=VERSION
+    version=VERSION,
+    python_requires='>=3.6',
+    install_requires=[
+        'torch>=1.5',
+        'pytorch_lightning==0.8.5',
+        'transformers==2.11.0',
+        'requests',
+        'nltk',
+        'tqdm',
+        'lxml'
+    ]
 )
-
-
-
